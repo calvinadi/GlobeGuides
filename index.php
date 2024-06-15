0b14600501a70fb7d038805d5b4d501b82b52272
calvinadi <150777674+calvinadi@users.noreply.github.com>
2024-06-15 13:30:34 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GlobeGuides - Travel & Tour</title>
+    
+    <!-- swiper css link-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <!-- font cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+<body>
+<!-- header section start -->
+
+<section class="header">
+    <a href="home.php" class="logo">GlobeGuides</a>
+    
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="services.php">services</a>
+        <a href="book.php">book</a>
+        <a href="contact.php">contact</a>
+        <a href="login.php">login</a>
+        <a href="signup.php">signup</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section end -->
+
+    
+<!--footer section start -->
+
+<section class="footer">
+    <div class="box-container">
+        
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"><i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+            <a href="services.php"><i class="fas fa-angle-right"></i>services</a>
+            <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+            <a href="contact.php"><i class="fas fa-angle-right"></i>contact</a>
+            <a href="login.php"><i class="fas fa-angle-right"></i>login</a>
+            <a href="signup.php"><i class="fas fa-angle-right"></i>signup</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+
+        <div class="box">
+            <h3>contacts info</h3>
+            <a href="#"><i class="fas fa-phone"></i> +123-456-7890</a>
+            <a href="#"><i class="fas fa-envelope"></i> cs@globeguides.com</a>
+            <a href="#"><i class="fas fa-map"></i> Jakarta, Indonesia</a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fas fa-facebook-f"></i> Facebook</a>
+            <a href="#"><i class="fas fa-twitter-f"></i> Twitter</a>
+            <a href="#"><i class="fas fa-instagram-f"></i> Instagram</a>
+            <a href="#"><i class="fas fa-linkedin-f"></i> Linkedin</a>
+        </div>
+
+<!--footer section end -->
+
+<!--swiper js link-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file link-->
+<script src="js/script.js"></script>
+
+</body>
+
+</html>
